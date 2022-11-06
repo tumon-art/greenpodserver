6fd38203966f8df6304b3149c10186d81d4be3d4
tumon <tumonkhn@gmail.com>
2022-11-06 11:26:09 +0600

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import router from "./routers";
+const cors = require("cors");
 
 const app = express();
 app.use(router);
+
+// CORS CONFIG
+const corsConfig = {
+  credentials: true,
+  origin: true,
+};
+
+app.use(express.json());
+
+// CORS
+app.use(cors(corsConfig));
 
 app.listen(process.env.PORT || 8080, () => console.log("listening 8080"));
